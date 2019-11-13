247be06c9abc79de156b4372330692d7c5a702c9
Krishnamurthy Pradeep <pradeeepmurthy@gmail.com>
2019-11-13 12:55:39 +0530

    Add sum logic, controller and unit test

--- myapp-spring-jenkins-docker/src/test/java/com/myapp/spring/MyappSpringJenkinsDockerApplicationTests.java
+++ myapp-spring-jenkins-docker/src/test/java/com/myapp/spring/MyappSpringJenkinsDockerApplicationTests.java
@@ -1,4 +1,4 @@
-package myappspringjenkinsdocker.myappspringjenkinsdocker;
+package com.myapp.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
